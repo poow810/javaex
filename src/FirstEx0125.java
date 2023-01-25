9b2acbc840f36f52fe0e217a08a4d993ca6e35b9
poow810 <poow810@naver.com>
2023-01-25 18:22:07 +0900

    day1homework

--- src/FirstEx0125.java
+++ src/FirstEx0125.java
@@ -1,8 +1,66 @@
 public class FirstEx0125 {
     public static void main (String[] args) {
+        int a = 5;
+
+        System.out.printf("a=%d\n", a);           // printf = f를 꼭써야함 (format함수)
         System.out.println("HELLO JAVA");
-        System.out.print("HELLO JAVA");
-        System.out.print("HELLO JAVA");
-        System.out.printf();
+        System.out.println("HELLO JAVA");
+        System.out.println("HELLO JAVA");
+        System.out.printf("DecimalNum=%d\n" , 7);   // %d = decimal number
+
+        double dv = 3.14 ;      // double은 실수 type   8bite의 빈 공간 할당
+        float fv = 3;           // float는 실수 type   4bite의 빈 공간 할당
+        // 3.14를 float type로 인식하게 만드는 법 ( casting ) = f를 붙여줌
+        float NUMBER = 3.14f;
+        float TWO = (float)3.14;
+        System.out.printf("TWO=" + TWO + "\n");    // 뒤 TWO를 문자열로 바꿔버림 = 문자열 + 문자열
+        System.out.printf("fv = %f\n", fv);
+        System.out.printf("fv = %.2f\n", fv);      // %.x 소수점 이하 x번째 자리까지만 출력
+        System.out.printf("fv = %07.2f\n", fv);    // 전체 자리를 총 7자리 확보 0003.00 소수점 포함 7자리
+        System.out.printf("fv = %7.2f\n", fv);     // (+) 7자리를 확보하되, 오른쪽 정렬, 정수부 0은 표기 x
+        System.out.printf("fv = %-7.2f\n", fv);    // (-) 7자리 확보하되, 왼쪽 정렬
+        System.out.println(NUMBER);
+        System.out.printf("fv = %f\n", fv);
+        System.out.printf("dv = %f\n", dv);
+
+        /* byte, bit
+        1 byte = 8bit
+        1 byte는 256가지의 수 표현...
+        */
+
+        // 문자열 data type
+        char ch = 'A';
+        System.out.printf("ch=%c\n", ch);           // %d : 정수로 해석, %c : 문자로 해석
+        System.out.printf("ch=%d\n", (int)ch);      // 대문자 A에 대한 ascii code 값 = 10진수로 표현
+
+/*      char ch2 = "A";          작은 따옴표 -> 문자 하나(character로 취급) / 큰 따옴표로 묶은 문자 -> 문자열(string으로 인식)
+                                 java에서 string을 null문자로 끝나야함 -> 대문자 A와 NULL 각각 2Byte를 집어넣기위해 총 4Byte필요
+                                 ch는 2Byte. 따라서 컴파일 오류 => char를 string으로 바꿔주면 됨
+ */
+
+        String name = "gildong";
+        System.out.println("name=" + name);
+        System.out.printf("name=%s\n", name);
+
+
+        // var 예약어 - data type을 추론
+        var ab = 100;
+        System.out.println(ab);
+
+        var ac = "안녕";
+        System.out.println(ac);
+
+        // final - 변경할 수 없는 변수
+        final double PI = 3.14 ;
+        System.out.printf("PI=%.2f\n", PI);
+
+        // 연산자
+        int s = 15/2 ;      // 소수점을 제외한 몫
+        int p = 13%2 ;      // 나머지
+        System.out.printf("s=%d, p=%d\n", s,p);
+
+        int m = 3;
+        m++;
+        System.out.println("m = " + m);
     }
 }
--- src/day1homework.java
+++ src/day1homework.java
@@ -1,45 +1,65 @@
 public class day1homework {
     public static void main(String[] args) {
-        // 문제 1번
-        char a = 'a'; //A.
-        char b = "a"; //B.
-        String c = "a"; //C.
-        String d = 'a'; //D.
-        char e = 'ab'; //E.
-        String f = "ab"; //F.
-        // B : 큰 따옴표안를 쓰면 문자열로 인식하기때문에 null값으로 끝나 2Byte의 char type에는 문자열을 넣을 수 없다.
-        // D : 작은 따옴표로 묶은 것은 문자 리터럴로 유니코드로 변환되어 저장되며 문자 하나로 인식한다.
-        // E : 위와 마찬가지이다
-        // 정답 B, D, E
+//        // 문제 1번
+//        char a = 'a'; //A.
+//        char b = "a"; //B.
+//        String c = "a"; //C.
+//        String d = 'a'; //D.
+//        char e = 'ab'; //E.
+//        String f = "ab"; //F.
+//        // B : 큰 따옴표안를 쓰면 문자열로 인식하기때문에 null값으로 끝나 2Byte의 char type에는 문자열을 넣을 수 없다.
+//        // D : 작은 따옴표로 묶은 것은 문자 리터럴로 유니코드로 변환되어 저장되며 문자 하나로 인식한다.
+//        // E : 위와 마찬가지이다
+//        // 정답 B, D, E
+//
+//        // 문제 2번
+//        byte var1 = 128; //A.
+//        short var2 = 128; //B.
+//        int var3 = 28L; //C.
+//        long var4 = 128L; //D.
+//        float var5 = 123456.789123; //E.
+//        double var6 = 123456.789123; //F.
+//        // A : byte는 8bit로 0부터 127까지의 128가지로 표현할 수 있다. 128은 범위밖이다.
+//        // C : 28은 int 타입 범위 내이므로 L을 붙일 필요가 없음. 붙이고 싶다면 int를 long으로 바꿔준다.
+//        // E : 실수 리터럴을 기본적으로 double type로 인식하기 때문에 맨 뒤에 f를 붙여 float type으로 인식할 수 있도록 해주어야한다.
+//        // 정답 A, C, E
+//
+//        // 문제 3번
+//        byte var1 = 127;
+//        short var2 = 128;
+//        int var3 = 128;
+//        long var4 = 128L;
+//        var4 = var1;
+//        System.out.println(var1 + ", " + var2);
+//        var1 = (byte) var3;
+//        System.out.println(var1 + ", " + var3);
+//
+//        float var5 = 123456.789123f;
+//        double var6 = 123456.789123;
+//        var5 = (float) var6;
+//        System.out.println(var5 + ", " + var6);
+//        var6 = var5;
+//        System.out.println(var5 + ", " + var6);
 
-        // 문제 2번
-        byte var1 = 128; //A.
-        short var2 = 128; //B.
-        int var3 = 28L; //C.
-        long var4 = 128L; //D.
-        float var5 = 123456.789123; //E.
-        double var6 = 123456.789123; //F.
-        // A : byte는 8bit로 0부터 127까지의 128가지로 표현할 수 있다. 128은 범위밖이다.
-        // C : 28은 int 타입 범위 내이므로 L을 붙일 필요가 없음. 붙이고 싶다면 int를 long으로 바꿔준다.
-        // E : 실수 리터럴을 기본적으로 double type로 인식하기 때문에 맨 뒤에 f를 붙여 float type으로 인식할 수 있도록 해주어야한다.
-        // 정답 A, C, E
+//        int i1 = 0x11, i2 = 5;
+//        final int ONE = 1;
+//        char c1 = 'a';
+//        float f1 = 1.5f;
+//        double d1 = 2.8;
+//        boolean b1 = true;
+//        System.out.printf("i1=%d\n", i1);
+//        System.out.printf("i1/2=%d\n", i1 / 2);
+//        System.out.println("c1 + ONE= " + c1 + ONE);
+//        System.out.println("(int)c1 + ONE= " + (int) c1 + ONE);
+//        System.out.printf("(c1 + ONE)=%d\n", c1 + ONE);
+//        System.out.printf("(c1 + ONE)=%c\n", c1 + ONE);
+//        System.out.printf("(c1 + ONE)=%5s\n", c1 + ONE);
+//        System.out.printf("i2 + f1=%f\n", i2 + f1);
+//        System.out.printf("i1 + d1=%.1f\n", f1 + d1);
+//        System.out.printf("(int)(f1 + d1)=%d\n", (int) (f1 + d1));
+//        System.out.printf("(int)f1 + (int)d1=%d\n", (int) f1 + (int) d1);
+//        System.out.println("b1 = " + b1);
 
-        // 문제 3번
-        byte var1 = 127;
-        short var2 = 128;
-        int var3 = 128;
-        long var4 = 128L;
-        var4 = var1;
-        System.out.println(var1 + ", " + var2);
-        var1 = (byte) var3;
-        System.out.println(var1 + ", " + var3);
-
-        float var5 = 123456.789123f;
-        double var6 = 123456.789123;
-        var5 = (float) var6;
-        System.out.println(var5 + ", " + var6);
-        var6 = var5;
-        System.out.println(var5 + ", " + var6);
 
     }
 }
