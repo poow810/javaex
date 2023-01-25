a72d856ce11c45448c1d38280e7fa2420bef06c8
poow810 <poow810@naver.com>
2023-01-25 13:12:51 +0900

    java day01

---
+++ src/FirstEx0125.java
@@ -0,0 +1,8 @@
+public class FirstEx0125 {
+    public static void main (String[] args) {
+        System.out.println("HELLO JAVA");
+        System.out.print("HELLO JAVA");
+        System.out.print("HELLO JAVA");
+        System.out.printf();
+    }
+}
